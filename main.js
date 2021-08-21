93667096623b49dc942c83a40756c0c5022a8fd9
Sonic <74811261+Sonic-Igtm@users.noreply.github.com>
2021-08-21 15:31:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,29 @@
-leftWristX = 0;
-rightWristX = 0;
-
-
+function draw(){
+background('#969A97');
+fill('#1B5E20');    
+textSize(difference);
+text('Vihaan', 50, 400);    
+}
 
 function setup(){
 canvas = createCanvas(430, 430);
 canvas.position(630, 100);
 video = createCapture(VIDEO);
 video.size(500, 500);
-
 posenet = ml5.poseNet(video, modelLoaded);
-posenet.on('pose', gotPoses );
 }
 
 function modelLoaded(){
-console.log("Posenet is Intialized!");
+console.log('Posenet is Intialized!');
 }
 
 function gotPoses(results){
-    console.log(results);
+    if(results.length > 0){
+        console.log(results);
+
+    leftWristX=results[0].pose.leftWrist.x;
+    rightWristX=results[0].pose.rightWrist.y;
+    difference = floor(leftWristX - rightWristX);
+    
+      }   
 }
