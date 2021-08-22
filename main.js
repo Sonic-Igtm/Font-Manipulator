a73a833fcc4c33963ab0e9496a9d8ba0ac445c21
Sonic <74811261+Sonic-Igtm@users.noreply.github.com>
2021-08-22 09:46:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,15 +6,17 @@ function draw(){
 background('#969A97');
 fill('#1B5E20');    
 textSize(difference);
-text('Vihaan', 50, 400);    
+text('Vihaan', 30, 300);
+document.getElementById("font_size").innerHTML = "Size of the font will be =" + difference + "px"       
 }
 
 function setup(){
 canvas = createCanvas(430, 430);
-canvas.position(630, 100);
+canvas.position(630, 130);
 video = createCapture(VIDEO);
 video.size(500, 500);
 posenet = ml5.poseNet(video, modelLoaded);
+posenet.on('pose', gotPoses );
 }
 
 function modelLoaded(){
@@ -25,9 +27,9 @@ function gotPoses(results){
     if(results.length > 0){
         console.log(results);
 
-    leftWristX=results[0].pose.leftWrist.x;
-    rightWristX=results[0].pose.rightWrist.x;
-    difference=floor(leftWristX - rightWristX);
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor(leftWristX - rightWristX);
     
       }   
 }
